2d83b7c3e8ea546256391a2137de7e5fb0cfbaf1
rasool <fallah.rasol@gmail.com>
2021-11-02 21:23:46 +0330

    Separation of different parts:Panel

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -10,41 +10,11 @@
     <link rel="stylesheet" href="/admin/css/font.css">
 </head>
 <body>
-<div class="sidebar__nav border-top border-left  ">
-    <span class="bars d-none padding-0-18"></span>
-    <a class="header__logo  d-none" href=""></a>
-    <div class="profile__info border cursor-pointer text-center">
-        <div class="avatar__img"><img src="/admin/img/pro.jpg" class="avatar___img">
-            <input type="file" accept="image/*" class="hidden avatar-img__input">
-            <div class="v-dialog__container" style="display: block;"></div>
-            <div class="box__camera default__avatar"></div>
-        </div>
-        <span class="profile__name">کاربر : احمد محمدی</span>    </div>
+
+    @include('admin.layout.aside')
 
 
-    <ul>
-        <li class="item-li i-dashboard is-active"><a href="index.html">پیشخوان</a></li>
-        <li class="item-li i-courses "><a href="courses.html">دوره ها</a></li>
-        <li class="item-li i-users"><a href="users.html"> کاربران</a></li>
-        <li class="item-li i-categories"><a href="categories.html">دسته بندی ها</a></li>
-        <li class="item-li i-slideshow"><a href="slideshow.html">اسلایدشو</a></li>
-        <li class="item-li i-banners"><a href="banners.html">بنر ها</a></li>
-        <li class="item-li i-articles"><a href="articles.html">مقالات</a></li>
-        <li class="item-li i-ads"><a href="ads.html">تبلیغات</a></li>
-        <li class="item-li i-comments"><a href="comments.html"> نظرات</a></li>
-        <li class="item-li i-tickets"><a href="tickets.html"> تیکت ها</a></li>
-        <li class="item-li i-discounts"><a href="discounts.html">تخفیف ها</a></li>
-        <li class="item-li i-transactions"><a href="transactions.html">تراکنش ها</a></li>
-        <li class="item-li i-checkouts"><a href="checkouts.html">تسویه حساب ها</a></li>
-        <li class="item-li i-checkout__request "><a href="checkout-request.html">درخواست تسویه </a></li>
-        <li class="item-li i-my__purchases"><a href="mypurchases.html">خرید های من</a></li>
-        <li class="item-li i-notification__management"><a href="notification-management.html">مدیریت اطلاع رسانی</a>
-        </li>
-        <li class="item-li i-user__inforamtion"><a href="user-information.html">اطلاعات کاربری</a></li>
-    </ul>
-
-</div>
-<div class="content">
+    <div class="content">
     <div class="header d-flex item-center bg-white width-100 border-bottom padding-12-30">
         <div class="header__right d-flex flex-grow-1 item-center">
             <span class="bars"></span>
@@ -178,6 +148,6 @@
 
 </div>
 </body>
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/js.js"></script>
+<script src="/admin/js/jquery-3.4.1.min.js"></script>
+<script src="/admin/js/js.js"></script>
 </html>
